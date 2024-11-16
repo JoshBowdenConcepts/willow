788a77137f692f7e1cd9566595869be5979cf702
Josh Bowden <joshbowdenconcepts@gmail.com>
2024-11-15 21:09:56 -0500

    Got Styles Working in Package

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import willowTheme from "./WillowTheme";
+
+addons.setConfig({
+  theme: willowTheme,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(), // Inject CSS into JavaScript
+  ],
   build: {
     lib: {
       entry: "./src/index.ts",
