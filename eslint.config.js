0df7fa4dfe0b6246f386e919c9b34e398c54fbfb
Josh Bowden <joshbowdenconcepts@gmail.com>
2025-04-23 08:49:05 -0400

    Added prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,50 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    // Global ignores: not scoped to any file globs
-    ignores: [
-      'dist',
-      '**/dist/**',
-      'node_modules',
-      '**/node_modules/**',
-      '**/.next/**'
-    ],
-  },
-  {
-    files: ['./{packages,apps}/**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react,
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      'react/prop-types': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      '@typescript-eslint/no-wrapper-object-types': 'off'
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-];
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		// Global ignores: not scoped to any file globs
+		ignores: [
+			'dist',
+			'**/dist/**',
+			'node_modules',
+			'**/node_modules/**',
+			'**/.next/**',
+		],
+	},
+	{
+		files: ['./{packages,apps}/**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+			react,
+			'react-hooks': reactHooks,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': ['warn'],
+			'react/prop-types': 'off',
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn',
+			'@typescript-eslint/no-wrapper-object-types': 'off',
+			'prettier/prettier': ['error'],
+		},
+		settings: {
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+]
