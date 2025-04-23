1fb857f85c62676a621217dd2a35ee48821ac9cd
Josh Bowden <joshbowdenconcepts@gmail.com>
2025-04-22 20:49:06 -0400

    Updated app

--- apps/personal/src/app/page.tsx
+++ apps/personal/src/app/page.tsx
@@ -1,47 +1,52 @@
-import styles from "./page.module.css";
-import { Text, Box, Link } from "components";
-import { yearsSinceStartingDev } from "./layout";
+import styles from './page.module.css'
+import { Text, Box, Button } from 'components'
+import { yearsSinceStartingDev } from './layout'
 
 export default function Home() {
-  return (
-    <div>
-      <main className={styles.main}>
-        <Box padding={[0, 1200]} display="flex" alignItems="center">
-          <Box
-            maxWidth={"700px"}
-            padding={[1400, 0]}
-            display="flex"
-            alignItems="center"
-            gap={600}
-          >
-            <Text as="h1" align="center">
-              Hello 👋, I'm Josh Bowden, a Design Systems Architect with
-              {" " + yearsSinceStartingDev} years of experience helping
-              companies create consistent design through development.
-            </Text>
-            <Box maxWidth={"650px"}>
-              <Text as="p" align="center" color="fgSubtle">
-                From infrastructure to governance, I am passionate about making
-                the complex dynamic between design and engineering easier and
-                more efficient. Does your company need help? My experience from
-                companies including <strong>GitHub</strong>,{" "}
-                <strong>American Express</strong>, and <strong>CarGurus</strong>{" "}
-                have taught me the skills to help no matter where you are in
-                your design system journey.
-              </Text>
-            </Box>
-            <Box display="grid" gridAutoFlow="column" gap={200}>
-              <Link href="/about" type="button">
-                Let's Talk
-              </Link>
-              <Link href="#" type="button" variant="secondary">
-                Learn More
-              </Link>
-            </Box>
-          </Box>
-        </Box>
-      </main>
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+	return (
+		<div>
+			<main className={styles.main}>
+				<Box padding={[0, 1200]} display="flex" alignItems="center">
+					<Box
+						maxWidth={'700px'}
+						padding={[1400, 0]}
+						display="flex"
+						alignItems="center"
+						gap={600}
+					>
+						<Text as="h1" align="center">
+							Hello 👋, I'm Josh Bowden, a Design Systems
+							Architect with
+							{' ' + yearsSinceStartingDev} years of experience
+							helping companies create consistent design through
+							development.
+						</Text>
+						<Box maxWidth={'650px'}>
+							<Text as="p" align="center" color="fgSubtle">
+								From infrastructure to governance, I am
+								passionate about making the complex dynamic
+								between design and engineering easier and more
+								efficient. Does your company need help? My
+								experience from companies including{' '}
+								<strong>GitHub</strong>,{' '}
+								<strong>American Express</strong>, and{' '}
+								<strong>CarGurus</strong> have taught me the
+								skills to help no matter where you are in your
+								design system journey.
+							</Text>
+						</Box>
+						<Box display="grid" gridAutoFlow="column" gap={200}>
+							<Button as="a" href="/about">
+								Let's Talk
+							</Button>
+							<Button as="a" href="/about">
+								Learn More
+							</Button>
+						</Box>
+					</Box>
+				</Box>
+			</main>
+			<footer className={styles.footer}></footer>
+		</div>
+	)
 }
--- packages/components/lib/Button/Button.tsx
+++ packages/components/lib/Button/Button.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-import { clsx } from "clsx";
-import { button_root } from "./Button.module.css";
+import React from 'react'
+import { clsx } from 'clsx'
+import styles from './Button.module.css'
 
 type BaseProps = {
-  children?: React.ReactNode;
-  variant?: "primary" | "secondary" | "outline" | "link";
-};
+	children?: React.ReactNode
+	variant?: 'primary' | 'secondary' | 'outline' | 'link'
+}
 
 type ButtonTagProps = BaseProps & {
-  as?: "button";
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+	as?: 'button'
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 type AnchorTagProps = BaseProps & {
-  as: "a";
-} & React.AnchorHTMLAttributes<HTMLAnchorElement>;
+	as: 'a'
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>
 
-export type ButtonProps = ButtonTagProps | AnchorTagProps;
+export type ButtonProps = ButtonTagProps | AnchorTagProps
 
 export const Button = (props: ButtonProps) => {
-  const { as = "button" } = props;
+	const { as = 'button' } = props
 
-  if (as === "a") {
-    const { children, variant, className, ...rest } = props as AnchorTagProps;
-    return (
-      <a className={clsx(className, button_root)} {...rest}>
-        {children}
-      </a>
-    );
-  }
+	if (as === 'a') {
+		const { children, variant, className, ...rest } =
+			props as AnchorTagProps
+		return (
+			<a className={clsx(className, styles.button_root)} {...rest}>
+				{children}
+			</a>
+		)
+	}
 
-  const { children, variant, className, ...rest } = props as ButtonTagProps;
-  return (
-    <button className={clsx(className, button_root)} {...rest}>
-      {children}
-    </button>
-  );
-};
+	const { children, variant, className, ...rest } = props as ButtonTagProps
+	return (
+		<button className={clsx(className, styles.button_root)} {...rest}>
+			{children}
+		</button>
+	)
+}
